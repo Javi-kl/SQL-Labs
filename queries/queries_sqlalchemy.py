1ef91b2cdb87eb0c4a13fde7ed2e6be0380b8c8b
Javi-kl <203809377+Javi-kl@users.noreply.github.com>
2026-01-27 20:04:52 +0100

    feat: Agregar nuevas queries mediante sqlalchemy

--- queries/queries_sqlalchemy.py
+++ queries/queries_sqlalchemy.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select, func
 from scripts.sql_alch_ecom import engine, Customer, Order, Product, Session
+from sqlalchemy.orm import joinedload
 
 
 def queries_execute():
@@ -19,6 +20,26 @@ def queries_execute():
             select(func.count()).select_from(Order).where(Order.state == "Pendiente")
         )
         print(f"Pedidos en 'Pendiente': {res}")
+
+        print(
+            "\n--- 4.  Busca el cliente con el id=1. Luego, sin hacer una nueva query explícita, imprime la fecha de todos sus pedidos."
+        )
+        c = session.get(Customer, 1)
+        if c:
+            print(f"Fecha para los pedidos del cliente: {c.name}")
+            for order in c.orders:
+                print(f" {order.order_date}")
+
+        print(
+            "Encuentra todos los pedidos cuyo total sea superior a 100€. Muestra el ID del pedido y el nombre del cliente que lo hizo."
+        )
+        res = session.scalars(
+            select(Order).where(Order.total > 100).options(joinedload(Order.customer))
+        ).all()
+
+        print(f"Pedidos encontrados: {len(res)}")
+        for o in res:
+            print(f"Id pedido: {o.id} | Cliente: {o.customer.name}")
 
 
 if __name__ == "__main__":
